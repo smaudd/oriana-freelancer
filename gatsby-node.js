1c18819f6b91de434789e81c6ea2ae70425f9b36
smaudd <smauddproject@gmail.com>
2019-04-04 19:21:18 +0200

    New components

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,4 @@
-const path = require('path');
+import path from 'path';
 
 exports.createPages = ({actions, graphql}) => {
     const { createPage } = actions
--- src/pages/services.js
+++ src/pages/services.js
@@ -1,24 +1,10 @@
-// import React from 'react'
-// import Link from 'gatsby-link'
+import React from 'react'
+import Link from 'gatsby-link'
 
-// const Navbar = () => (
-//             <navbar style={{
-//                 background: '#F4F4F4',
-//                 paddingTop: '10px'
-//             }}>
-//                 <ul style={{
-//                     listStyle: 'none',
-//                     display: 'flex',
-//                     justify: 'space-evenly'
-//                 }}>
-//                     <li><Link to="/home">Home</Link></li>
-//                     <li><Link to="/sbout">About</Link></li>
-//                     <li><Link to="/services">Services</Link></li>
-//                     <li><Link to="/calendar">Calendar</Link></li>
-//                     <li><Link to="/blog">Blog</Link></li>
-//                 </ul>
-                
-//             </navbar>
-// )
+const Services = () => (
+            <h1>
+                Our services...
+            </h1>
+)
 
-// export default Navbar
+export default Services
