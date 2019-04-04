adf173291ae8c9a34c6c0c9626192f664915bd3a
smaudd <smauddproject@gmail.com>
2019-04-04 21:27:06 +0200

    Blog posts transition

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Link from 'gatsby-link';
 import Layout from '../components/layout';
 import { graphql } from 'gatsby';
+import transition from './page-transitions.module.css'
 
 export default function Template({ data }) {
     const post = data.markdownRemark
 
     return (
         <Layout>
-            <div>
+            <div className={transition.all}>
                 <Link to="/blog">Go Back</Link>
                 <hr />
                 <h1>{post.frontmatter.title}</h1>
